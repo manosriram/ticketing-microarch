953154b4db537a8e97cd04ef7af3314221b7dd7e
Mano Sriram <mano.sriram0@gmail.com>
2020-08-16 13:35:11 +0530

    Update common

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -2,6 +2,9 @@ import request from 'supertest'
 import { app } from '../../app';
 import { Ticket } from '../../../models/ticket';
 
+// To create a new objectId (for testing)
+// const id = new mongoose.Types.ObjectId().toHexString();
+
 it('has a route handler listening to /api/tickets/ for post requests', async () => {
     const response = await request(app).post('/api/tickets').send({});
 
@@ -14,10 +17,54 @@ it('can only be accessed if the user is signed in', async () => {
 
 it('creates a ticket with valid inputs', async () => {
     let tickets = await Ticket.find({});
-    expect(tickets.length).toEqual(0);
+    const title = '21313123sd';
 
     await request(app).post('/api/tickets').set('Cookie', global.signin()).send({
-        title: '1231312as',
+        title,
         price: 12
-    }).expect(201);
+    });
+
+    tickets = await Ticket.find({});
+    expect(tickets.length).toEqual(1);
+    expect(tickets[0].price).toEqual(12);
+    expect(tickets[0].title).toEqual(title);
+});
+
+it('returns an error if an invalid title is provided', async () => {
+    await request(app)
+        .post('/api/tickets/')
+        .set('Cookie', global.signin())
+        .send({
+            title: '',
+            price: 10
+        })
+        .expect(400);
+
+    await request(app)
+        .post('/api/tickets/')
+        .set('Cookie', global.signin())
+        .send({
+            price: 10
+        })
+        .expect(400);
+});
+
+
+it('returns an error if an invalid price is provided', async () => {
+    await request(app)
+        .post('/api/tickets/')
+        .set('Cookie', global.signin())
+        .send({
+            title: 'sdajsld',
+            price: -10
+        })
+        .expect(400);
+
+    await request(app)
+        .post('/api/tickets/')
+        .set('Cookie', global.signin())
+        .send({
+            title: 'llkajsd'
+        })
+        .expect(400);
 });
--- tickets/src/routes/create.ts
+++ tickets/src/routes/create.ts
@@ -2,12 +2,21 @@ import express, { Request, Response } from 'express';
 import { requireAuth, validateRequest } from '@microarch-ticketing/common';
 import { body } from 'express-validator';
 const router = express.Router();
+import { Ticket } from '../../models/ticket';
 
 router.post("/api/tickets/", requireAuth, [
     body('title').not().isEmpty().withMessage('Title is required'),
     body('price').isFloat({ gt: 0 }).withMessage('Price must be greater than 0')
 ], validateRequest, async (req: Request, res: Response) => {
-    res.send({});
+    const { title, price } = req.body;
+    const ticket = Ticket.build({
+        title,
+        price,
+        userId: req.currentUser!.id
+    });
+    await ticket.save();
+
+    res.status(201).send(ticket);
 });
 
 export { router as createTicket };
