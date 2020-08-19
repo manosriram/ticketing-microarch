ee7d5bb5ceab5ceff9fa881ec9e2a30060e661bb
Mano Sriram <mano.sriram0@gmail.com>
2020-08-19 14:36:03 +0530

    Start Nats-Streaming-Service

---
+++ nats-test/src/listener.ts
@@ -0,0 +1,24 @@
+import nats, { Message } from 'node-nats-streaming';
+import { randomBytes } from 'crypto';
+console.clear();
+
+const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
+    url: 'http://localhost:4222'
+});
+
+stan.on('connect', () => {
+    console.log('Listener Connected!');
+
+    const options = stan.subscriptionOptions().setManualAckMode(true);
+    const subscription = stan.subscribe('ticket:created', 'listenerQueueGroup', options);
+
+    subscription.on('message', (msg: Message) => {
+        const data = msg.getData();
+
+        if (typeof data === 'string') {
+            console.log(`Received event #${msg.getSequence()}, with data ${data}`);
+        }
+
+        msg.ack();
+    });
+});
